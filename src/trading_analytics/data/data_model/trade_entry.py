955ea651adefd6cac6a9370ba415da7b5dbd79bf
violetdiamonds <violetdiamonds@protonmail.com>
2025-06-25 17:05:05 -0400

    Adding Enum class for trading strategy.

--- src/trading_analytics/data/data_model/trade_entry.py
+++ src/trading_analytics/data/data_model/trade_entry.py
@@ -36,6 +36,27 @@ class TradeAction(str, Enum):
     SOLD_CLOSE = 'SOLD CLOSE'
 
 # ToDo: Enum for Strategy
+# Enum for valid strategies
+class TradeStrategy(str, Enum):
+    """Enum class for valid trading strategies.
+
+    This class defines a set of valid trading strategies as string enumerations.
+    Each strategy represents a specific type of trading approach.
+
+    Attributes:
+        DIVIDEND (str): Dividend-based trading strategy.
+        ETF (str): Exchange-traded fund trading strategy.
+        BASIC_TRADE (str): Standard stock trading strategy.
+        BASIC_OPTION (str): Basic options trading strategy.
+        COVERED_CALL (str): Covered call options trading strategy.
+        SPREAD (str): Spread-based options trading strategy.
+    """
+    DIVIDEND = 'DIVIDEND'
+    ETF = 'ETF'
+    BASIC_TRADE = 'BASIC TRADE'
+    BASIC_OPTION = 'BASIC OPTION'
+    COVERED_CALL = 'COVERED CALL'
+    SPREAD = 'SPREAD'
 
 class TradeEntry(BaseModel):
     """A model representing a trade entry with relevant details.
