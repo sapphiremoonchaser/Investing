644d5c7604ec3aabce172505a8b4220ce6b2c1ee
violetdiamonds <violetdiamonds@protonmail.com>
2025-06-26 14:34:03 -0400

    added validator to make sure correct security type

--- src/trading_analytics/data/data_model/stock_entry.py
+++ src/trading_analytics/data/data_model/stock_entry.py
@@ -1,8 +1,30 @@
 # Dataclass for Stocks
 
 # Imports
-from pydantic import Field
-from src.trading_analytics.data.data_model.trade_entry import TradeEntry
+from pydantic import Field, model_validator
+from src.trading_analytics.data.data_model.trade_entry import TradeEntry, SecurityType
 
 class StockEntry(TradeEntry):
     price_per_share: float = Field(ge=0, frozen=True)
+
+    # Add a model validator to ensure security is 'STOCK' or 'INDEX'
+    @model_validator(mode='after')
+    def check_stock_security(self):
+        """Validates that the security type is either 'STOCK' or 'INDEX'.
+
+        We already validated the action in TradeEntry.
+
+        Args:
+            self: The model instance being validated.
+
+        Returns:
+            self: The validated model instance.
+
+        Raises:
+            ValueError: If the security type is not 'STOCK' or 'INDEX'.
+        """
+        valid_securities = {SecurityType.STOCK, SecurityType.INDEX}
+        if self.security not in valid_securities:
+            raise ValueError(f"StockEntry must have security type in {valid_securities}. Got {type(self.security)}")
+
+        return self
