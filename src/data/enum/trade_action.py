626b0a42b121cb89403082f7a321ed2cc8ad2fe1
violetdiamonds <violetdiamonds@protonmail.com>
2025-07-10 22:24:01 -0400

    updating trade action enum

--- src/data/enum/trade_action.py
+++ src/data/enum/trade_action.py
@@ -9,25 +9,17 @@ class TradeAction(str, Enum):
     Each action represents a specific type of trading activity.
 
     Attributes:
-        BOUGHT (str): Standard purchase of a security.
-        BOUGHT_COVER (str): Purchase to cover a short position.
-        BOUGHT_OPEN (str): Purchase to open a position, typically for options.
+        BUY (str): Standard purchase of a security.
         DIVIDEND (str): Dividend payment received.
         OPTION_EXPIRED (str): Option contract expired.
         OPTION_ASSIGNED (str): Option contract assigned.
         OPTION_EXERCISED (str): Option contract exercised.
-        SOLD (str): Standard sale of a security.
-        SOLD_SHORT (str): Sale to open a short position.
-        SOLD_CLOSE (str): Sale to close a position, typically for options.
+        SELL (str): Standard sale of a security.
     """
-    BOUGHT = 'BOUGHT'
-    BOUGHT_COVER = 'BOUGHT COVER'
-    BOUGHT_OPEN = 'BOUGHT OPEN'
+    BUY = 'BUY'
     DIVIDEND = 'DIVIDEND'
     OPTION_EXPIRED = 'OPTION EXPIRED'
     OPTION_ASSIGNED = 'OPTION ASSIGNED'
     OPTION_EXERCISED = 'OPTION EXERCISED'
-    SOLD = 'SOLD'
-    SOLD_SHORT = 'SOLD SHORT'
-    SOLD_CLOSE = 'SOLD CLOSE'
+    SELL = 'SELL'
     
